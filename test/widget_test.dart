6c7d8cdbaeb5af8887c410b65e35db210aa5954a
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-04-16 15:31:22 -0500

    base testing created

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -26,7 +26,7 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);*/
-    expect(false, true);
+    expect(true, true);
   });
   
 }
