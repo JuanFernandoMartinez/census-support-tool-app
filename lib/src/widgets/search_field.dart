2aafd2c39922e3dbcad581965da57173e9633e0c
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-04-18 08:51:30 -0500

    Updated searchfield style

--- lib/src/widgets/search_field.dart
+++ lib/src/widgets/search_field.dart
@@ -2,21 +2,33 @@
 
 import 'package:flutter/material.dart';
 
+
 class SearchField extends StatelessWidget{
   const SearchField({super.key});
   
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          width: MediaQuery.of(context).size.width*0.7,
-          child: TextField()
+          width: MediaQuery.of(context).size.width*0.8,
+          decoration: BoxDecoration(color: const  Color(0xffD5CFE1),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [BoxShadow(color: Colors.black, blurRadius: 1.5, spreadRadius: 0.5, offset: Offset(0, 0) )],
+          ),
+
+          child: Row( mainAxisAlignment: MainAxisAlignment.center ,children: [
+            Container(
+          width: MediaQuery.of(context).size.width*0.6,
+          child:   const TextField( ),
         ),
-        Icon(Icons.search),
-        Icon(Icons.filter_alt)
+        const Icon(Icons.search, size: 35,),
+          ],),
+        ),
+        const Icon(Icons.filter_alt, size: 35,)
       ],
-      mainAxisAlignment: MainAxisAlignment.center,
+      
     );
   }
   
