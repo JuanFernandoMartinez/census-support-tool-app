f54f51e8bdd52722550663e6a51903923ab7cbe3
Brayan Ortiz~ <65927857+BrayanOB2003@users.noreply.github.com>
2024-04-28 21:26:02 -0500

    fix: Category with more fidelity to mockups

--- lib/src/screens/form_application/form_sheet.dart
+++ lib/src/screens/form_application/form_sheet.dart
@@ -13,37 +13,57 @@ class FormSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 5,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      clipBehavior: Clip.none,
-      color: const Color(0xFFDFFFEA),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Form(
-            child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              category,
-              style: const TextStyle(fontFamily: 'Inria Sans', fontSize: 25),
+    return Column(children: [
+      SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 15, 0, 20),
+          child: Material(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
             ),
-            Flexible(
-                child: ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: formFields.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [formFields[index], const SizedBox(height: 10)],
-                );
-              },
-            ))
-          ],
-        )),
+            clipBehavior: Clip.none,
+            color: const Color(0xFFDFFFEA),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                category,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontFamily: 'Inria Sans', fontSize: 25),
+              ),
+            ),
+          ),
+        ),
       ),
-    );
+      Material(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        clipBehavior: Clip.none,
+        color: const Color(0xFFDFFFEA),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                  child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: formFields.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [formFields[index], const SizedBox(height: 10)],
+                  );
+                },
+              ))
+            ],
+          )),
+        ),
+      ),
+    ]);
   }
 }
